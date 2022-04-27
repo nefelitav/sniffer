1624b1de36846ffa8de53d0b67e0c7675497abe4
nefelitav <nefeli.tavoulari123@gmail.com>
2022-04-27 23:48:32 +0300

    add bash script

---
+++ finder.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# for each TLD
+for arg in "$@"  
+do
+    count=0
+    # for each .out file in /tmp
+    for file_path in /tmp/*.out; do 
+        # read each line of the file
+        while read line; do
+            # get first word of line -> location
+            url=$(echo "$line" | awk '{print $1;}')
+            # get second word of line -> occurences
+            occurences=$(echo "$line" | awk '{print $2;}')
+            echo $occurences
+            # tokenize by . , get the last token and compare to the given TLD
+            if [ ${url##*.} = $arg ] 
+            then
+                # increment counter
+                count=$((count+$occurences))
+            fi
+        done <${file_path}
+    done
+    echo "$arg found $count times";
+done
